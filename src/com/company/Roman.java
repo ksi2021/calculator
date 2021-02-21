e94687f9724a9b82bdc1be4cb5db04aa72957e32
ksi2021 <77726265+ksi2021@users.noreply.github.com>
2021-02-21 15:47:28 +0700

    Update Roman.java

--- src/com/company/Roman.java
+++ src/com/company/Roman.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class Roman extends Base {
+        R_val test = new R_val();
         Roman(String expression){
             super(expression);
         }
@@ -13,4 +14,29 @@ public class Roman extends Base {
                 default -> 0;
             };
         }
+
+    public String get_rez(){
+            Integer result = null;
+         switch (arr[1]) {
+            case "+" -> {result = Integer.parseInt(map.get(arr[0])) + Integer.parseInt(map.get(arr[2]));break;}
+            case "-" -> {result = Integer.parseInt(map.get(arr[0])) - Integer.parseInt(map.get(arr[2]));break;}
+             case "*" -> {result = Integer.parseInt(map.get(arr[0])) * Integer.parseInt(map.get(arr[2]));break;}
+             case "/" -> {result = Integer.parseInt(map.get(arr[0])) / Integer.parseInt(map.get(arr[2]));break;}
+
+         };
+         if(result != null){
+             if(result > 0)
+             {
+                 return test.values.get(result.toString());
+             }
+             else if(result < 0)
+             {
+                 result = result * -1;
+                 return test.values.get((result).toString());
+             }
+             else if(result == 0) { return  "0";}
+
+         }
+        return "Ошибка вычисления";
+    }
 }
